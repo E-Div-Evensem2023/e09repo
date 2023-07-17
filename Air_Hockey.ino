d44131d046517673933157452e00cfafea105fd7
SRIHARI K MANCHALI <131451150+Srihari-Km@users.noreply.github.com>
2023-07-17 09:10:39 +0530

    Add files via upload

--- Air_Hockey.ino
+++ Air_Hockey.ino
@@ -1,118 +1,110 @@
 #include <LiquidCrystal_I2C.h>
-#include <Servo.h>
 
-// IR Sensor 1
-const int irSensor1Pin = 2;
+// Pin Definitions
+#define LEFT_BUTTON_PIN   2
+#define RIGHT_BUTTON_PIN  3
+#define IR_SENSOR_1_PIN   4
+#define IR_SENSOR_2_PIN   5
+#define MOTOR_PIN1 8
+#define MOTOR_PIN2 9
 
-// IR Sensor 2
-const int irSensor2Pin = 3;
+// Constants
+#define WINNING_SCORE 5
 
-// Servo Motor 1
-const int servo1Pin = 4;
-Servo servo1;
+// Global Variables
+int strikerScore = 0;
+int defenderScore = 0;
 
-// Servo Motor 2
-const int servo2Pin = 5;
-Servo servo2;
-
-// Reset Switch
-const int resetSwitchPin = 6;
-
-// LCD Display
-LiquidCrystal_I2C lcd(0x27, 16, 2);
-
-// Scores
-int score1 = 0;
-int score2 = 0;
-
-// Variables for tracking state
-bool irSensor1Activated = false;
-bool irSensor2Activated = false;
-bool resetButtonPressed = false;
-
-// Function to update LCD display
-void updateLCD() {
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("Score1: ");
-  lcd.print(score1);
-
-  lcd.setCursor(0, 1);
-  lcd.print("Score2: ");
-  lcd.print(score2);
-}
-
-// Function to handle servo movement
-void moveServo(Servo& servo, int angle) {
-  servo.write(angle);
-  delay(2000);
-  servo.write(0);
-}
+// Initialize LCD
+LiquidCrystal_I2C lcd(0x27, 16, 2);  // I2C address may vary, use LCD's address
 
 void setup() {
+  // Initialize Serial
+  Serial.begin(9600);
+
+  // Initialize LCD
   lcd.begin(16, 2);
-  lcd.backlight();
+  lcd.print("Striker: 0   ");
+  lcd.setCursor(0, 1);
+  lcd.print("Defender: 0   ");
 
-  servo1.attach(servo1Pin);
-  servo2.attach(servo2Pin);
-
-  pinMode(irSensor1Pin, INPUT);
-  pinMode(irSensor2Pin, INPUT);
-  pinMode(resetSwitchPin, INPUT_PULLUP);
-
-  updateLCD();
+  // Initialize Pins
+  pinMode(LEFT_BUTTON_PIN, INPUT_PULLUP);
+  pinMode(RIGHT_BUTTON_PIN, INPUT_PULLUP);
+  pinMode(IR_SENSOR_1_PIN, INPUT);
+  pinMode(IR_SENSOR_2_PIN, INPUT);
+  pinMode(MOTOR_PIN1, OUTPUT);
+  pinMode(MOTOR_PIN2, OUTPUT);
 }
 
-
 void loop() {
-  // Check if IR Sensor 1 is activated
-  if (digitalRead(irSensor1Pin) == LOW && !irSensor1Activated) {
-    score1++;
-    irSensor1Activated = true;
-    moveServo(servo1, 90);
-    updateLCD();
-  }
-  else if (digitalRead(irSensor1Pin) == HIGH) {
-    irSensor1Activated = false;
+  // Read button states
+  bool isLeftButtonPressed = digitalRead(LEFT_BUTTON_PIN) == LOW;
+  bool isRightButtonPressed = digitalRead(RIGHT_BUTTON_PIN) == LOW;
+
+  // Move motor based on button states
+  if (isLeftButtonPressed) {
+    moveMotorLeft();
+  } else if (isRightButtonPressed) {
+    moveMotorRight();
+  } else {
+    stopMotor();
   }
 
-  // Check if IR Sensor 2 is activated
-  if (digitalRead(irSensor2Pin) == LOW && !irSensor2Activated) {
-    score2++;
-    irSensor2Activated = true;
-    moveServo(servo2, 90);
-    updateLCD();
-  }
-  else if (digitalRead(irSensor2Pin) == HIGH) {
-    irSensor2Activated = false;
-  }
+  // Check if ball passes sensors
+  bool isBallPassedSensor1 = digitalRead(IR_SENSOR_1_PIN) == HIGH;
+  bool isBallPassedSensor2 = digitalRead(IR_SENSOR_2_PIN) == HIGH;
 
-  // Check if Reset button is pressed
-  if (digitalRead(resetSwitchPin) == LOW && !resetButtonPressed) {
-    score1 = 0;
-    score2 = 0;
-    resetButtonPressed = true;
-    updateLCD();
-  }
-  else if (digitalRead(resetSwitchPin) == HIGH) {
-    resetButtonPressed = false;
+  // Update scores and display
+  if (isBallPassedSensor1 && isBallPassedSensor2) {
+    strikerScore++;
+    updateScoreDisplay(strikerScore, defenderScore);
+  } else if (isBallPassedSensor1) {
+    defenderScore++;
+    updateScoreDisplay(strikerScore, defenderScore);
   }
 
-  // Check if any score reaches 7
-  if (score1 >= 7 || score2 >= 7) {
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("Game Over!");
-
-    lcd.setCursor(0, 1);
-    if (score1 >= 7) {
-      lcd.print("Player 1 wins!");
-    } else {
-      lcd.print("Player 2 wins!");
-    }
-
+  // Check for game over condition
+  if (strikerScore >= WINNING_SCORE || defenderScore >= WINNING_SCORE) {
+    displayWinner();
     while (true) {
-      // Do nothing, the game has ended
+      // Stay in an infinite loop to prevent further execution
     }
+  }
+
+  delay(100);  // Delay for stability
+}
+
+void moveMotorLeft() {
+  digitalWrite(8, HIGH);
+  digitalWrite(9, LOW);
+  Serial.println("Moving motor left");
+}
+
+void moveMotorRight() {
+  digitalWrite(9, HIGH);
+  digitalWrite(8, LOW);
+  Serial.println("Moving motor right");
+}
+
+void stopMotor() {
+  // Code to stop the motor
+  // Replace with your implementation
+  Serial.println("Stopping motor");
+}
+
+void updateScoreDisplay(int strikerScore, int defenderScore) {
+  lcd.setCursor(9, 0);
+  lcd.print(strikerScore);
+  lcd.setCursor(9, 1);
+  lcd.print(defenderScore);
+}
+
+void displayWinner() {
+  lcd.clear();
+  if (strikerScore >= WINNING_SCORE) {
+    lcd.print("Striker wins!");
+  } else {
+    lcd.print("Defender wins!");
   }
 }
